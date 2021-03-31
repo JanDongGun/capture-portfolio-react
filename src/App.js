fc37ee812e6596d70a555c89f03f3bc66fa0127c
jandonggun <donggontrannguyen@gmail.com>
2021-03-31 15:22:58 +0700

    our work page route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
 import OurWork from "./pages/OurWork";
 import ContactUs from "./pages/ContactUs";
+import MovieDetail from "./pages/MovieDetails";
 import { Switch, Route } from "react-router-dom";
 function App() {
   return (
@@ -14,8 +15,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
         </Route>
         <Route path="/contact">
           <ContactUs />
